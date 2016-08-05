83cd8d0f473464e9ba9188253cee7e8dfd0f2255
Timothy Johnson <timsteele@Timothys-MacBook-Air.local>
2016-08-04 19:27:20 -0700

    lower case keys

--- airtablemethods.js
+++ airtablemethods.js
@@ -1,5 +1,5 @@
 var Airtable = require('airtable');
-var base = new Airtable({ apiKey: PROCESS.ENV.ATKEY }).base('app6wA7UTiPFvDybn');
+var base = new Airtable({ apiKey: process.env.ATKEY }).base('app6wA7UTiPFvDybn');
 var speakersList = [];
 var supportersList = [];
 var aboutParagraphs = [];
